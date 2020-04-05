78722f4f1c6e3a2091e6da2a2ab714b1525eff5f
Aaron <tim2464@yahoo.com.tw>
2020-04-05 16:22:07 -0700

    [Lab8] Part 1

---
+++ iOS-Student-PizzaRestaurant-master/PizzaRestaurant/Pizza.h
@@ -0,0 +1,30 @@
+//
+//  Pizza.h
+//  PizzaRestaurant
+//
+//  Created by AaronH on 2020-04-04.
+//  Copyright © 2020 Lighthouse Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSUInteger, PizzaSize) {
+    SMALL,
+    MEDIUM,
+    LARGE,
+    UNDEFINED = NSUIntegerMax
+};
+
+@interface Pizza : NSObject
+
+@property (nonatomic, readonly, assign) PizzaSize size;
+@property (nonatomic, readonly, strong) NSArray * toppings;
+
+- (instancetype)initWithSize:(PizzaSize)size AndToppings:(NSArray *)toppings;
+
++ (Pizza *)pepperoniWithSize:(PizzaSize)size;
++ (Pizza *)meatLoversWithSize:(PizzaSize)size;
+
++ (PizzaSize)sizeFromString:(NSString *)sizeString;
+
+@end
